aad1963f26b10415e15262ae9c50269a6be8829c
Tobias Blum <github@tobiasblum.de>
2019-01-02 14:33:13 +0100

    FS Webserver

--- src/main.cpp
+++ src/main.cpp
@@ -81,7 +81,9 @@ RCSwitch rc_switch = RCSwitch();
 // *************************************************
 // Init: ESPWebServer
 // *************************************************
+#include <FS.h>
 ESP8266WebServer server(80);
+File fsUploadFile;
 
 
 // *************************************************
@@ -465,6 +467,179 @@ void sendStatusJSON()
     server.send(200, "application/json", response);
 }
 
+String getContentType(String filename)
+{
+    if (server.hasArg("download"))
+        return "application/octet-stream";
+    else if (filename.endsWith(".htm"))
+        return "text/html";
+    else if (filename.endsWith(".html"))
+        return "text/html";
+    else if (filename.endsWith(".css"))
+        return "text/css";
+    else if (filename.endsWith(".js"))
+        return "application/javascript";
+    else if (filename.endsWith(".png"))
+        return "image/png";
+    else if (filename.endsWith(".gif"))
+        return "image/gif";
+    else if (filename.endsWith(".jpg"))
+        return "image/jpeg";
+    else if (filename.endsWith(".ico"))
+        return "image/x-icon";
+    else if (filename.endsWith(".xml"))
+        return "text/xml";
+    else if (filename.endsWith(".pdf"))
+        return "application/x-pdf";
+    else if (filename.endsWith(".zip"))
+        return "application/x-zip";
+    else if (filename.endsWith(".gz"))
+        return "application/x-gzip";
+    return "text/plain";
+}
+
+bool handleFileRead(String path)
+{
+    Serial.println("handleFileRead: " + path);
+    if (path.endsWith("/"))
+        path += "index.htm";
+    String contentType = getContentType(path);
+    String pathWithGz = path + ".gz";
+    if (SPIFFS.exists(pathWithGz) || SPIFFS.exists(path))
+    {
+        if (SPIFFS.exists(pathWithGz))
+            path += ".gz";
+        File file = SPIFFS.open(path, "r");
+        size_t sent = server.streamFile(file, contentType);
+        file.close();
+        return true;
+    }
+    return false;
+}
+
+void handleFileUpload()
+{
+    if (server.uri() != "/edit")
+        return;
+    HTTPUpload &upload = server.upload();
+    if (upload.status == UPLOAD_FILE_START)
+    {
+        String filename = upload.filename;
+        if (!filename.startsWith("/"))
+            filename = "/" + filename;
+        Serial.print("handleFileUpload Name: ");
+        Serial.println(filename);
+        fsUploadFile = SPIFFS.open(filename, "w");
+        filename = String();
+    }
+    else if (upload.status == UPLOAD_FILE_WRITE)
+    {
+        Serial.print("handleFileUpload Data: ");
+        Serial.println(upload.currentSize);
+        if (fsUploadFile)
+            fsUploadFile.write(upload.buf, upload.currentSize);
+    }
+    else if (upload.status == UPLOAD_FILE_END)
+    {
+        if (fsUploadFile)
+            fsUploadFile.close();
+        Serial.print("handleFileUpload Size: ");
+        Serial.println(upload.totalSize);
+    }
+}
+
+void handleMinimalUpload()
+{
+    char temp[1500];
+
+    snprintf(temp, 1500,
+             "<!DOCTYPE html>\
+        <html>\
+        <head>\
+            <title>ESP8266 Upload</title>\
+            <meta charset=\"utf-8\">\
+            <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\
+            <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\
+        </head>\
+        <body>\
+            <form action=\"/edit\" method=\"post\" enctype=\"multipart/form-data\">\
+            <input type=\"file\" name=\"data\">\
+            <input type=\"text\" name=\"path\" value=\"/\">\
+            <button>Upload</button>\
+            </form>\
+        </body>\
+        </html>");
+    server.sendHeader("Access-Control-Allow-Origin", "*");
+    server.send(200, "text/html", temp);
+}
+
+void handleFileList()
+{
+    if (!server.hasArg("dir"))
+    {
+        server.send(500, "text/plain", "BAD ARGS");
+        return;
+    }
+
+    String path = server.arg("dir");
+    Serial.println("handleFileList: " + path);
+    Dir dir = SPIFFS.openDir(path);
+    path = String();
+
+    String output = "[";
+    while (dir.next())
+    {
+        File entry = dir.openFile("r");
+        if (output != "[")
+            output += ',';
+        bool isDir = false;
+        output += "{\"type\":\"";
+        output += (isDir) ? "dir" : "file";
+        output += "\",\"name\":\"";
+        output += String(entry.name()).substring(1);
+        output += "\"}";
+        entry.close();
+    }
+
+    output += "]";
+    server.send(200, "text/json", output);
+}
+
+void handleFileDelete()
+{
+    if (server.args() == 0)
+        return server.send(500, "text/plain", "BAD ARGS");
+    String path = server.arg(0);
+    Serial.println("handleFileDelete: " + path);
+    if (path == "/")
+        return server.send(500, "text/plain", "BAD PATH");
+    if (!SPIFFS.exists(path))
+        return server.send(404, "text/plain", "FileNotFound");
+    SPIFFS.remove(path);
+    server.send(200, "text/plain", "");
+    path = String();
+}
+
+void handleFileCreate()
+{
+    if (server.args() == 0)
+        return server.send(500, "text/plain", "BAD ARGS");
+    String path = server.arg(0);
+    Serial.println("handleFileCreate: " + path);
+    if (path == "/")
+        return server.send(500, "text/plain", "BAD PATH");
+    if (SPIFFS.exists(path))
+        return server.send(500, "text/plain", "FILE EXISTS");
+    File file = SPIFFS.open(path, "w");
+    if (file)
+        file.close();
+    else
+        return server.send(500, "text/plain", "CREATE FAILED");
+    server.send(200, "text/plain", "");
+    path = String();
+}
+
+
 
 // *************************************************
 // SETUP
@@ -472,9 +647,10 @@ void sendStatusJSON()
 void setup()
 {
     // start serial port
-    Serial.begin(9600);
+    Serial.begin(115200);
     Serial.println("Pumpduino v2");
 
+    SPIFFS.begin();
 
     // ========================================
     // Initializing OLED display
@@ -599,12 +775,6 @@ void setup()
     // ========================================
     // Initializing WebServer
     // ========================================
-    server.onNotFound([]() {
-        server.send(404, "text/plain", "404 not found");
-    });
-    server.on("/", []() {
-        sendStatusJSON();
-    });
     server.on("/get", []() {
         sendStatusJSON();
     });
@@ -695,6 +865,24 @@ void setup()
             commitSettings();
         }
         sendStatusJSON();
+    });
+    server.on("/upload", handleMinimalUpload);
+    server.on("/edit", HTTP_POST, []() { server.send(200, "text/plain", ""); }, handleFileUpload);
+    server.on("/list", HTTP_GET, handleFileList);
+    server.on("/edit", HTTP_GET, []() {
+        if (!handleFileRead("/edit.htm"))
+        {
+            server.send(404, "text/plain", "FileNotFound");
+        }
+    });
+    server.on("/edit", HTTP_PUT, handleFileCreate);
+    server.on("/edit", HTTP_DELETE, handleFileDelete);
+
+    server.onNotFound([]() {
+        if (!handleFileRead(server.uri()))
+        {
+            server.send(404, "text/plain", "404 file not found");
+        }
     });
 
     server.begin();
